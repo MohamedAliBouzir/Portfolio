dcdcb3372fc1d6117cf0b5489781c82108b8fe7d
MohamedAliBouzir <bzr.medali@gmail.com>
2024-12-13 08:44:48 +0100

    Fixing dependencies imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App/App.tsx";
+import "tw-elements-react/dist/css/tw-elements-react.min.css";
 import "./styles/general.css";
 import { ThemeContextProvider } from "./contexts/themeContext.tsx";
-import "tw-elements-react/dist/css/tw-elements-react.min.css";
 
 const children = (
   <ThemeContextProvider>
